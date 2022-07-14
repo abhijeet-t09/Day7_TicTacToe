bfe998c63d8ce3a441f2cf126fef6dbd2a79a127
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-07-14 15:43:30 +0530

    Player Choice Updated

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -4,8 +4,19 @@ import java.util.*;
 public class TicTacToe {
 
     static char[] board;
+    static char choice;
+
+    static char chooseLetter(){
+        Scanner sc=new Scanner(System.in);
+        char letter=sc.next().charAt(0);
+        return letter;
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to TicTacToe Game !!");
         board=new char[10];
+
+        System.out.print("Select X 0r O: ");
+        choice=chooseLetter();
+        System.out.println("You Chose "+choice+"!!");
     }
 }
